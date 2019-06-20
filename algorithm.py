67ed39b828476eb9c040d04de5d64e92579c6bc3
Caroline <38522055+carolinechoi@users.noreply.github.com>
2019-06-19 18:49:46 -0700

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -8,6 +8,17 @@ def convertTemp(file_name):
     # setting up a counter method
     file_num = 0
     column_num = 1
+    new_number = 1
+    # setting up our max/min
+    max_number_c = 0
+    max_number_f = 0
+    min_number_c = 1000
+    min_number_f = 1000
+    # setting up averages
+    avg_sum_c = 0
+    avg_sum_f = 0
+    avg_number_c = 0
+    avg_number_f = 0
     for line in fileObj:
         file_num = file_num + 1
         if file_num < 7:
@@ -19,8 +30,11 @@ def convertTemp(file_name):
             line = line.strip()
             listical = line.split(" ")
             for number in listical:
+                # continuing with temperature conversion
                 numberCelsius = float(number) - 273.15
                 numberFarenheit = numberCelsius * 9 / 5 + 32
+                avg_sum_c = avg_sum_c + numberCelsius
+                avg_sum_f = avg_sum_f + numberFarenheit
                 if column_num % cols == 0:
                     celsiusFileObj.write(str(numberCelsius) + '\n')
                     farenheitFileObj.write(str(numberFarenheit) + '\n')
@@ -28,9 +42,28 @@ def convertTemp(file_name):
                     celsiusFileObj.write(str(numberCelsius) + " ")
                     farenheitFileObj.write(str(numberFarenheit) + " ")
                 column_num = column_num + 1
+                # to find the maximum values 
+                if numberCelsius > max_number_c:
+                    max_number_c = numberCelsius
+                if numberFarenheit > max_number_f:
+                    max_number_f = numberFarenheit
+                # to find the minimum values
+                if numberCelsius < min_number_c:
+                    min_number_c = numberCelsius
+                if numberFarenheit < min_number_f:
+                    min_number_f = numberFarenheit
+                new_number = new_number + 1
+            avg_number_c = avg_sum_c / new_number
+            avg_number_f = avg_sum_f / new_number
     celsiusFileObj.close()
+    print("The maximum temperature (Celsius) of the " + file_name + " file is " + str(max_number_c))
+    print("The maximum temperature (Farenheit) of the " + file_name + " file is " + str(max_number_f))
+    print("The minimum temperature (Celsius) of the " + file_name + " file is " + str(min_number_c))
+    print("The minimum temperature (Farenheit) of the " + file_name + " file is " + str(min_number_f))
+    print("The average temperature (Celsius) of the " + file_name + " file is " + str(avg_number_c))
+    print("The average temperature (Farenheit) of the " + file_name + " file is " + str(avg_number_f))
 
 # calling 
-convertTemp('k_20181007.txt')
-convertTemp('k_20181108.txt')
-convertTemp('k_20190127.txt')
+convertTemp('k_20181007_test.txt')
+convertTemp('k_20181108_test.txt')
+convertTemp('k_20190127_test.txt')
